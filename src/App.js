f8925cdd4f9515ae7dacee33008cda53b2b6563b
jv512 <successlife217@gmail.com>
2021-12-27 07:11:02 -0500

    add task list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Task from "./Task";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Task
+        task={{
+          id: "1",
+          title: "Test Task",
+          state: "TASK_PINNED",
+          updatedAt: new Date(2021, 0, 1, 9, 0),
+        }}
+      />
     </div>
   );
 }
